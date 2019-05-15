b0358b13a9b14f1f7b01f3d006ff805d3296cbff
kevin.yang <kevin.yang@all-dream.com>
2019-05-15 14:18:00 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,8 @@
+'use strict';
+require('should');
+const LoaderX = require('../src/loaderx');
+describe('LoaderX Test', () => {
+  it('should get "LoaderX"', () => {
+    console.log(LoaderX)
+  });
+});
